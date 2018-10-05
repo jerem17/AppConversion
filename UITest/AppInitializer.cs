c7655f77c4469d9edb8d0531b08f4517c200fdbe
Jerem <jeremy.hoffmann@epitech.eu>
2018-10-05 09:46:28 +0200

    add device identifier param

--- UITest/AppInitializer.cs
+++ UITest/AppInitializer.cs
@@ -18,7 +18,7 @@ namespace UITest
        //           return ConfigureApp.Android.DeviceSerial("420088e7f4f1b300").ApkFile("E:/Xamarin Stage/AppConversionUITest/AppTest/AppTest/AppTest.Android/bin/Debug/com.companyname.AppTest.apk").StartApp();
 
             }
-            return ConfigureApp.iOS.AppBundle("/Users/Shared/Jenkins/Home/workspace/build_project/AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
+            return ConfigureApp.iOS.DeviceIdentifier("4D74D7C0-101F-4012-B27C-DA158B6691CD").AppBundle("/Users/Shared/Jenkins/Home/workspace/build_project/AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
         }
     }
 }
