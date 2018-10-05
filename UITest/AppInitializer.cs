29c954ac2c5ef62e9101b20ccbefb252fbccdec0
Jerem <jeremy.hoffmann@epitech.eu>
2018-10-05 16:20:48 +0200

    remove device identifier

--- UITest/AppInitializer.cs
+++ UITest/AppInitializer.cs
@@ -18,7 +18,7 @@ namespace UITest
        //           return ConfigureApp.Android.DeviceSerial("420088e7f4f1b300").ApkFile("E:/Xamarin Stage/AppConversionUITest/AppTest/AppTest/AppTest.Android/bin/Debug/com.companyname.AppTest.apk").StartApp();
 
             }
-            return ConfigureApp.iOS.DeviceIdentifier("EC495EB6-C9AA-45B9-A71D-567A48134979").AppBundle("./AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
+            return ConfigureApp.iOS.AppBundle("./AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
             //           return ConfigureApp.iOS.AppBundle("~/Home/workspace/build_project/AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
             
         }
