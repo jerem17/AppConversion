7a68f6d6a2c7347a6739a65c7782579ecb749aa8
Jerem <jeremy.hoffmann@epitech.eu>
2018-10-09 10:39:05 +0200

    complete device identifier

--- UITest/AppInitializer.cs
+++ UITest/AppInitializer.cs
@@ -18,7 +18,7 @@ namespace UITest
        //           return ConfigureApp.Android.DeviceSerial("420088e7f4f1b300").ApkFile("E:/Xamarin Stage/AppConversionUITest/AppTest/AppTest/AppTest.Android/bin/Debug/com.companyname.AppTest.apk").StartApp();
 
             }
-            return ConfigureApp.iOS.DeviceIdentifier("3BA886C2-B528-4965-BEB").AppBundle("./AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
+            return ConfigureApp.iOS.DeviceIdentifier("3BA886C2-B528-4965-BEBD-EEFBAD5BC177").AppBundle("./AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
             //           return ConfigureApp.iOS.AppBundle("~/Home/workspace/build_project/AppTest/AppTest.iOS/bin/iPhoneSimulator/Debug/AppTest.iOS.app").PreferIdeSettings().StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
             
         }
