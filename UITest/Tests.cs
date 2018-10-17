cd0050e88fc87d01d9db2d2cc4061e3538cfcd35
Jerem <jeremy.hoffmann@epitech.eu>
2018-10-17 15:32:35 +0200

    clean code

--- UITest/Tests.cs
+++ UITest/Tests.cs
@@ -39,35 +39,30 @@ namespace UITest
             var appResult = app.Query("ResultLabel").First(result => result.Text == "5.8 $");
             app.Screenshot("Dollar_Conversion_Screen");
             Assert.IsTrue(appResult != null, "Dollar conversion don't work !");
-
         }
+
         [Test]
         public void Livre_Work_Conversion()
         {
-
             app.EnterText("ValueMoney", "580.23");
             app.Tap("LivreButton");
             var appResult = app.Query("ResultLabel").First(result => result.Text == "522.207 £");
             Assert.IsTrue(appResult != null, "Livre conversion don't work");
-            //app.Screenshot("livre screen");
+
             /*
-    FileInfo fileInfo = app.Screenshot("Dollar_screen");
-    string destinationPath = string.Format(@"/Users/Shared/Jenkins/Home/workspace/screenshot{0}", fileInfo.Name);
-
-    fileInfo.MoveTo(destinationPath);*/
-
+            FileInfo fileInfo = app.Screenshot("Dollar_screen");
+            string destinationPath = string.Format(@"/Users/Shared/Jenkins/Home/workspace/screenshot{0}", fileInfo.Name);
+            fileInfo.MoveTo(destinationPath);
+            */
         }
+
         [Test]
         public void Reset_Button_Work()
         {
-
             app.EnterText("ValueMoney", "87883");
             app.Tap("LivreButton");
             app.Tap("ResetButton");
             var appResult = app.Query(x => x.Marked("ValueMoney")?.Invoke("placeholder"))?.FirstOrDefault()?.ToString();
-         //   var appResult = "prout";
-            // app.Screenshot("reset screen");
-
             Assert.IsTrue(appResult != null, "Reset button don't work");            
         }
     }
